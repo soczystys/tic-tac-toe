bba60d1b4578abff91212c22ca7bb58c03dffc9b
soczystys <34406205+soczystys@users.noreply.github.com>
2023-07-28 17:26:32 +0200

    Added working solo game mode

--- src/main/java/org/example/SoloGameController.java
+++ src/main/java/org/example/SoloGameController.java
@@ -1,14 +1,24 @@
 package org.example;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import org.example.layer.data.Board;
+import org.example.layer.logic.CoordinateNotEmptyException;
 import org.example.layer.logic.MoveResolver;
+import org.example.layer.presentation.CommandLineInterfaceHandler;
+import org.example.layer.presentation.Coordinates;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.example.ControllerCommonMethodsHandler.*;
 
 public class SoloGameController {
 
@@ -22,13 +32,81 @@ public class SoloGameController {
     Board board = new Board();
     MoveResolver moveResolver = new MoveResolver();
     ControllerCommonMethodsHandler controllerCommonMethodsHandler = new ControllerCommonMethodsHandler();
+    CommandLineInterfaceHandler commandLineInterfaceHandler = new CommandLineInterfaceHandler();
 
     public void switchToMenuButtonClicked(ActionEvent actionEvent) throws IOException {
         controllerCommonMethodsHandler.switchToMenuButtonClicked(actionEvent);
     }
 
     public void clickedGridCell(MouseEvent mouseEvent) throws IOException {
-        controllerCommonMethodsHandler.clickedGridCell(mouseEvent, circleBoolean, moveResolver, board, myLabel, gridPane, 3);
+//        controllerCommonMethodsHandler.clickedGridCell(mouseEvent, circleBoolean, moveResolver, board, myLabel, gridPane, 3);
+//
+        var pane = ((Pane) mouseEvent.getSource());
+        var id = pane.getId();
+        var scene = pane.getScene();
+        char playerChar;
+
+//        if (circleBoolean) {
+            drawCircle(pane);
+            playerChar = 'o';
+//        } else {
+//            drawCross(pane);
+//            playerChar = 'x';
+//        }
+//        circleBoolean = !circleBoolean;
+
+//        Integer x = Integer.parseInt(String.valueOf(id.charAt(0)));
+//        Integer y = Integer.parseInt(String.valueOf(id.charAt(1)));
+
+//        Integer x = extractX(id);
+        Integer x = Integer.parseInt(id.split("-")[0]);
+        Integer y = Integer.parseInt(id.split("-")[1]);
+//        Integer y = extractY(id);
+
+//        TODO
+//
+        try {
+            moveResolver.resolveMove(board, new Coordinates(x, y), playerChar);
+        } catch (CoordinateNotEmptyException e) {
+
+        }
+
+        determineIfWon();
+
+        Coordinates coordinates = commandLineInterfaceHandler.pickRandomCoordinates(board);
+        moveResolver.resolveAutoMove(board, coordinates, 'x');
+        String paneIdString = coordinates.getX() + "-" + coordinates.getY();
+
+        ObservableList<Node> panes = gridPane.getChildren();
+        List<Node> paneInList = panes.stream().filter(node -> node.getId().equals(paneIdString))
+                .toList();
+
+        System.out.println("panes with id: " + paneInList.size());
+        System.out.println("scene: " + scene);
+        commandLineInterfaceHandler.showBoard(board);
+
+        Pane autoPane = (Pane) paneInList.get(0);
+        drawCross(autoPane);
+        determineIfWon();
+
+
+    }
+
+    private void determineIfWon() {
+        if (moveResolver.hasStreak(board, 'o', 3)) {
+            System.out.println("player with \'o\' won");
+            myLabel.setText("player with \'o\' won");
+
+            gridPane.setDisable(true);
+
+        } else if (moveResolver.hasStreak(board, 'x', 3)) {
+            System.out.println("player with \'x\' won");
+            myLabel.setText("player with \'x\' won");
+            gridPane.setDisable(true);
+        } else if (moveResolver.allCoordinatesTaken(board)) {
+            myLabel.setText("its a draw");
+            gridPane.setDisable(true);
+        }
     }
 
     public void mouseEntered(MouseEvent mouseEvent) {
